8161c34beea3525a8bd11f2b296a3ce20fbb61ed
AnghanPujan <poojan.anghan@exhibytesolution.com>
2025-08-22 17:29:25 +0530

    Validations Added

--- DRFApi/serializers.py
+++ DRFApi/serializers.py
@@ -15,3 +15,21 @@ class StudentSerializer(serializers.Serializer):
         instance.city = validated_data.get('city', instance.city)
         instance.save()
         return instance
+
+    def validate_roll(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Roll number must be greater than 0")
+        return value
+
+    def validate_name(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError("Name must only contain letters")
+        return value
+    
+    def validate_city(self, value):
+        if value is not str:
+            raise serializers.ValidationError("Invalid city name")
+        return value
+    
+    def validate(self, data):     # Data is the dictionary of the Field values
+        name = data.get
